f59337459bfad34aea8591d4cc28c1303bbe2f43
Marc Cirauqui <marc.cirauqui@abiquo.com>
2016-10-20 15:01:05 +0200

    Adds new setting to render links in email
    Setting ```email_render_links``` to false, the email template will
    not include the links to accept or deny the tasks.

--- app/mailers/tasks_mailer.rb
+++ app/mailers/tasks_mailer.rb
@@ -4,6 +4,7 @@ class TasksMailer < ApplicationMailer
     def send_approval_email(task_details, emails)
         to_line = emails.join(',')
         @tasks = task_details
+        @render_links = APP_CONFIG['email_render_links']
         mail(to: to_line, subject: APP_CONFIG['approval_subject'])
     end
 
