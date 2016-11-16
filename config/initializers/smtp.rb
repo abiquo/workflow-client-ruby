90239271182d23cc3a4b2604c56721bbc5920962
Marc Cirauqui <marc.cirauqui@abiquo.com>
2016-11-16 15:21:39 +0100

    Fix initializer and missing tag in template

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,15 +1,17 @@
 unless Rails.application.config.action_mailer.nil?
 
     options = YAML.load_file(Rails.root.join('config', 'config.yml'))[Rails.env]['smtp_settings']
-    
+
     Rails.application.config.action_mailer.raise_delivery_errors = true
     Rails.application.config.action_mailer.perform_deliveries = true
     Rails.application.config.action_mailer.delivery_method = :smtp
     Rails.application.config.action_mailer.smtp_settings = {}
-    Rails.application.config.action_mailer.default_url_options[:host] = options['email_link_host'] if options.key?['email_link_host']
-    Rails.application.config.action_mailer.default_url_options[:port] = options['email_link_port'] if options.key?['email_link_port']
-    Rails.application.config.action_mailer.default_url_options[:protocol] = options['email_link_protocol'] if options.key?['email_link_protocol']
-    Rails.application.config.action_mailer.default_url_options[:script_name] = options['email_link_path'] if options.key?['email_link_path']
+    Rails.application.config.action_mailer.default_url_options = {
+        host: options['email_link_host'],
+        port: options['email_link_port'],
+        protocol: options['email_link_protocol'],
+        script_name: options['email_link_path']
+    }
 
     options.each do |name, value|
         value = value.to_sym if value.to_s.starts_with? ':'
